6a688a578a38fab238a23132a45a2f4a8d073b0d
Yang <zyterence@icloud.com>
2020-05-29 11:51:50 +0800

    server setup

--- main.py
+++ main.py
@@ -4,3 +4,7 @@ from api.api import router
 app = FastAPI()
 app.include_router(router)
 
+
+if __name__ == "__main__":
+    import uvicorn
+    uvicorn.run("main:app", host="0.0.0.0", port=5000, log_level="info")
